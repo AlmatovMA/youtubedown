9a05d65c188b1e4576683691155195d4bbf8a919
AlmatovMA <108933777+AlmatovMA@users.noreply.github.com>
2023-03-09 19:52:08 +0800

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,6 +4,5 @@ from environs import Env
 env = Env()
 env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("5689175199:AAGqxdlr1Bne-qUcqv0eNnTNFV7tUkuvGis")  # Bot toekn
-ADMINS = env.list("5631918839")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
