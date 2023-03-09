4a74cd20242d81120223500cca2de0624ca64504
AlmatovMA <108933777+AlmatovMA@users.noreply.github.com>
2023-03-09 18:00:50 +0800

    Railway

---
+++ handlers/users/help.py
@@ -0,0 +1,14 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandHelp
+
+from loader import dp
+
+
+@dp.message_handler(CommandHelp())
+async def bot_help(message: types.Message):
+    text = ("нужна помошь?",
+            "команды: ",
+            "/start - запуск бота!",
+            "/help - помошь!")
+    
+    await message.answer("\n".join(text))
